d61c2c93b21769eb708ef66371c9693a0ae14edd
VITALIY1986 <staslego1986@gmail.com>
2023-07-12 13:31:16 +0300

    sadadgjsd

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,48 +1,24 @@
 import {Facebook, Instagram, Twitter, Youtube} from "./icons";
-
+import Link from 'next/link';
+import Image from 'next/image'
+import profilePic from '../../public/logo.png'
 const Footer = () => (
-	<div className="footer bg-purple p-6 text-white">
-		<div className="container mx-auto">
-			<div className="md:grid  md:grid-cols-6 gap-4 font-thin">
-				<div className="col-start-1 col-end-4 ">
-					<h3 className="mt-5 text-3xl text-blue">НАШІ КОМПАНІЇ</h3>
-					<div className=" flex mt-5 ">
-						<div className="flex-1 opacity-75 " >
-					     <p className="mb-3">Chantarelle</p>
-					     <p className="mb-3">Norel</p>
-					     <p className="mb-3">Dermaoxy</p>
-						 </div>
-						 <div className="flex-1 opacity-75">
-					     <p className="mb-3">Arkana</p>
-					     <p className="mb-3">Beyond Dental </p>
-					     <p className="mb-3">Sittara</p>
-						 </div>
-					</div>
-			    </div>
-				<div className="col-start-4 col-end-6 ">
-					<h3 className="mt-5 text-3xl text-blue ">КОНТАКТИ</h3>
-					<div className="mt-5 opacity-75 ">
-						<p>Mенеджер (Київ): +38 (097) 860 06 06 </p>
-						<p>Mенеджер (Київ): +38 (068) 706 70 67 </p>
-						<p>Mенеджер (регіони): +38 (097) 209 54 45</p>
-						<p>Mенеджер (Beyond): +38 (067) 328-74-26</p>
-						<p>м. Київ, провулок Західний 3Ц </p>
-						<p>info@4prof.com.ua</p>
-					</div>
-				</div>
-				<div className="col-start-6 col-end-6">
-				     <h3 className="mt-5 text-3xl text-blue">МИ В СОЦ.МЕРЕЖАХ</h3>
-				     <ul className="social-links flex  mt-5 ">
-				        <li><a href="https://www.facebook.com/4ProfUkraine" className="" target="_blank"><Facebook/></a></li>
-				        
-				        <li className="ml-2 mt-1"><a href="https://www.youtube.com/channel/UC3BBKd1UUUtXMjY4uL_NZ4g" className="fa fa-youtube" target="_blank"><Youtube/></a></li>
-				        <li className="ml-2"><a href="https://www.instagram.com/4profukraine/" className="fa fa-instagram" target="_blank"><Instagram/></a></li>
-			        </ul>
-			   </div>
-			</div>
+	<div className="footer bg-white shadow-2xl text-futer px-6 py-6 ">
+		<div className="flex flex-col items-center">
+		
+			<div className="w-24">
+	
+						<Image  src={profilePic} alt="C.O.S. Laboratories " />
+						</div>
+		
 			
+			<div className="flex mt-3">
+			<div className="bg-grey_cos  p-3 rounded-full mx-1 shadow-xl"><a href="https://www.facebook.com/COS.Laboratories/"><Facebook/></a></div><div className="bg-grey_cos  p-3 rounded-full mx-1 shadow-xl"><a href="https://www.instagram.com/cos_laboratories/"><Instagram/></a></div>
+			</div>
+			</div>
+			<div className=" mt-6 text-center"><p>© 2019-2022 C.O.S. Laboratories / Toate drepturile rezervate. </p></div>
 		</div>
-	</div>
+
 );
 
 export default Footer;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -136,172 +136,55 @@ const Nav = () => {
 		}
 	  
 	  ];
-	const Data = isLoggedIn ? questionsAnswersLog : questionsAnswers
-	const renderedQuestionsAnswers = Data.map((item, index) => {
-		const showDescription = index === activeIndex ? "show-description" : "";
-		const fontWeightBold = index === activeIndex ? "font-light " : "";
-		
-		const ariaExpanded = index === activeIndex ? "false" : "true";
-		return (
-			
-			
-			
-			<li  key={item.id}
-					aria-expanded={ariaExpanded}
-        aria-controls={`faq${index + 1}_desc`}
-        data-qa="faq__question-button"
-        className={`faq__question-button ${fontWeightBold} `}
 	
-		onClick={() => {
-			setActiveIndex(index);
-		  }}>
-					<a className="block">
-					{item.title	}<span className="caret ml-6"></span>
-				</a>
-				
-					<ul id={`faq${index + 1}_desc`}
-        data-qa="faq__desc"
-        className="">
-						<li className="block">
-						<Link href={item.linkhomepage	}>
-								<a className={`hidden ${showDescription}`}>
-								{item.title	} HOME
-								</a>
-							</Link>
-						<Link href={item.linkprof	}>
-								<a className={`${ item.linkprof === "" ? 'hidden' : `hidden ${showDescription}`}`}>
-								Професійна косметика
-								</a>
-							</Link>
-							<Link href={item.linkhome	}>
-								<a className={`${ item.linkhome === "" ? 'hidden' : `hidden ${showDescription}`}`}>
-								Домашня косметика
-								</a>
-							</Link>
-							
-							<Link  href={item.linkarticle	}>
-								<a className={`${ item.linkarticle === "" ? 'hidden' : `hidden ${showDescription}`}`} >
-								Статті
-								</a>
-							</Link>
-						</li>
-						
-						
-					</ul>
-					
-					</li>
-			
-			
-				
-				
-			
-		
-		);
-	  });
-	  const renderedQuestionsAnswersDesctop = Data.map((item, index) => {
-	
-		return (
-			
-			
-			<li className="nav-item relative   " key={item.id}>
-					<Link href={item.linkhomepage}>
-						<a className="block mt-4 lg:inline-block lg:mt-0 text-black hover:bg-blue hover:transition  delay-150 duration-300 ease-in-out ... hover:text-white transition-all cursor-pointer lead_hover px-3 py-5">
-						{item.title	} <span className="caret"></span>
-						</a>
-						</Link>
-						<ul className={`${ item.item === "" ? 'hidden' : 'dropdown-menus py-5 px-6 bg-blue mt-10  '}`}>
-							<li>
-							    <Link href={item.linkprof	}>
-						            <a className={`${ item.linkprof=== "" ? 'hidden' : 'block py-2 lg:inline-block lg:mt-0 font-thin text-lg text-purple-200  hover:text-white '}`}>
-									Професійна косметика
-									
-						            </a>
-					            </Link>
-							</li>
-							<li> 
-								<Link href={item.linkhome	}>
-						            <a className={`${ item.linkhome=== "" ? 'hidden' : 'block py-2 lg:inline-block lg:mt-0 font-thin text-lg text-purple-200  hover:text-white '}`}>
-						            Домашня косметика
-								
-						            </a>
-					            </Link>
-							</li>
-							<li> 
-								<Link href={item.linkarticle	}>
-						            <a className={`${ item.linkarticle === "" ? 'hidden' : 'block py-2 lg:inline-block lg:mt-0 font-thin text-lg text-purple-200  hover:text-white  '}`}>
-						            
-									Статті
-						            </a>
-					            </Link>
-							</li>
-							
-						</ul>
-						</li>
-			
-		
-		);
-	  });
 	return (
 
 		
-		<nav className="mx-2 ">
+		<nav className="mx-5 py-5 ">
 			<div className="flex items-center justify-between container mx-auto">
-
-				<div className="flex items-center flex-shrink-0 text-black mr-20 w-40">
+				<div className="flex items-center flex-shrink-0 text-black mr-20 w-24">
 					{/*<svg className="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>*/}
 					<Link href="/">
-					<Image  src={profilePic}  />
+						<Image  src={profilePic} alt="C.O.S. Laboratories" />
 					</Link>
 					<span className="font-semibold text-xl tracking-tight">
 					<Link href="/">
-					<a className=""></a>
+						<a className=""></a>
 					</Link>
 					</span>
 				</div>
-				<>
 		
-	
-			</>
-				{/*Menu button*/}
-				{/*<div>
-				<a href="/registration/register/" className="block  lg:hidden lg:mt-0 text-black hover:text-black  rounded-full p-2 bg-gray-200">
-						<svg xmlns="http://www.w3.org/2000/svg" className=" lg:block m-auto" fill="none" viewBox="0 0 24 24" width="18" height="auto" stroke="gray"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" /></svg>
-							
-						</a></div>*/}
+			
 							
 				<div className=" py-3 lg:hidden flex items-center">
 				<CartIconmob/>
 				<input id="hamburger" className="hamburger" type="checkbox"/>
 				<label className=" hamburger" for="hamburger">
-		  <i></i>
-		{/*  <span>
-			<span className="close">close</span>
-			<span className="open">menu</span>
-		  </span>*/}
+		  		<i></i>
+		
 		</label>
-		<section className="drawer-list bg-very_peri ">
-		<ul className="">
+		<section className="drawer-list bg-very_peri  ">
+		<ul className="text-center mt-30 uppercase">
+		<li className='mx-5 mt-10'><a href="/">Acasa</a></li>
+			<li className='mx-5 mt-10'><a href="/categories">produse</a></li>
+			<li className='mx-5 mt-10'><a href="/contact">Contact</a></li>
 			
-		{renderedQuestionsAnswers}
 		</ul>
 			</section>
 				</div>
 
 				{/*MMenu in mobile*/}
 				<div className={`${ isMenuVisible ? 'max-h-full h-full' : 'h-0' } w-full hidden lg:h-full flex-grow lg:flex lg:items-center lg:w-auto`}>
-					<ul className="text-sm font-medium  lg:flex-grow lg:flex h-full ">
-					{renderedQuestionsAnswersDesctop}
+					<ul className="text-sm font-medium  lg:flex-grow lg:flex h-full uppercase ">
+					<li className='mx-5'><a href="/">Acasa</a></li>
+					<li className='mx-5'><a href="/categories">produse</a></li>
+					<li className='mx-5'><a href="/contact">Contact</a></li>
+					
+					
 					</ul>
 					
 					<div className="text-sm font-medium">
-					{/*}	<a href="/registration/register/" className="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-black mr-10">
-						<svg xmlns="http://www.w3.org/2000/svg" className="hidden lg:block m-auto" fill="none" viewBox="0 0 24 24" width="18" height="auto" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" /></svg>
-							Profile
-						</a>
-						<a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-black mr-10">
-						<svg xmlns="http://www.w3.org/2000/svg" className="hidden lg:block m-auto" fill="none" viewBox="0 0 24 24" width="18" height="auto" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" /></svg>
-							Wishlist
-		</a>*/}
+					
 						<CartIcon/>
 					</div>
 				</div>

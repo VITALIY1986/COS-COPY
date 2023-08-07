6075a90de41d0ca57a8490f325dd8fc3ecbf2e55
VITALIY1986 <staslego1986@gmail.com>
2023-08-07 22:38:01 +0300

    asdfdsfas

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 
 const path = require("path");
 const allowedImageWordPressDomain = new URL(process.env.NEXT_PUBLIC_WORDPRESS_URL).hostname
-
+const withOptimizedImages = require('next-optimized-images');
 module.exports =   {
  
    

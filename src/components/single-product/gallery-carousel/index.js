30f52272c664af78e7fa701565fbd8a61fcf0e1c
VITALIY1986 <staslego1986@gmail.com>
2023-08-07 22:14:37 +0300

    asdfasdggasd

--- src/components/single-product/gallery-carousel/index.js
+++ src/components/single-product/gallery-carousel/index.js
@@ -67,6 +67,7 @@ const GalleryCarousel = ({gallery}) => {
                         )
                     })
                 }
+                <div className='slider-content'>
                 <div className="slider-button ">
                     <button className="focus:outline-none button-slider" onClick={nextSlide}>
                         <svg width="25px" className="inline-block " xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 16l-4-4m0 0l4-4m-4 4h18" /></svg>
@@ -75,6 +76,7 @@ const GalleryCarousel = ({gallery}) => {
                         <svg width="25px" className="inline-block" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 8l4 4m0 0l-4 4m4-4H3" /></svg>
                     </button>
                 </div>
+            </div>
             </div>
         </div>
     )
